fff411a7c4bf7ad032f7e8df4e1ba53411fbb968
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-08 03:17:09 -0300

    feat: :sparkles: requirement 2 complete

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW `estatisticas_musicais` AS
+  SELECT
+    COUNT(S.TITLE) `cancoes`,
+    (SELECT
+      COUNT(AR.FULL_NAME)
+    FROM
+      SpotifyClone.ARTISTS AR)  `artistas`,
+    (SELECT
+      COUNT(AL.TITLE)
+    FROM
+      SpotifyClone.ALBUMS AL) `albuns`
+  FROM
+      SpotifyClone.SONGS S;
+  
