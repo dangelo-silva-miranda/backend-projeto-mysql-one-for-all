e2330558de5495f7f727672f519c1fcb05225e84
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-09 23:41:54 -0300

    fix: :bug: fix to delete child rows before updating parent row

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-  AFTER DELETE ON SpotifyClone.USERS
+  BEFORE DELETE ON SpotifyClone.USERS
   FOR EACH ROW
 BEGIN
   DELETE FROM SpotifyClone.FOLLOWERS
